66205c6c86ce5e7d8ea77d7a0b45654bea6d1f1c
GHOSTrrxX <115501266+GHOSTrrxX@users.noreply.github.com>
2025-06-01 11:49:01 -0300

    Add files via upload

---
+++ Practico checkpoint7.js
@@ -0,0 +1,15 @@
+function calcular(num1, num2, num3, num4) {
+
+  const primerosDos = num1 + num2;
+  const SegundosDos = num3 + num4;
+
+  const Multiplicacion = sumaPrimerosDos * sumaSegundosDos;
+
+  if (Multiplicacion > 50) {
+    console.log(`El numero ${Multiplicacion} es mayor que 50`);
+
+  } else {
+    console.log(`El numero ${Multiplicacion} es menor que 50`);
+  }
+}
+calcular(3, 2, 5, 5);
